c58de8e640f4d1db3992fc6158c650e205f682ef
lee-ji-hong <zhffktkdlekghksxk@naver.com>
2022-12-18 16:07:00 +0900

    Feat: "모의고사 페이지 get api 연동 작업"

--- src/component/Exam/ExamList.js
+++ src/component/Exam/ExamList.js
@@ -1,13 +1,26 @@
+import { useState, useEffect } from 'react';
 import ContainerUI from '../UI/ContainerUI';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
-import Divider from  '@mui/material/Divider';
+import Divider from '@mui/material/Divider';
+import useHttpRequest from '../../hook/use-http';
+import LoadingSpinner from '../UI/LoadingSpinner';
 import { Link } from 'react-router-dom';
 
 // import { useNavigate } from 'react-router-dom';
 const ExamList = () => {
+  const [exam, setExam] = useState([]);
+  const { isLoading, sendGetRequest } = useHttpRequest();
   // const navigate = useNavigate();
+
+  useEffect(() => {
+    const examListHandler = data => {
+      setExam(data.data);
+      console.log(data.data);
+    };
+    sendGetRequest(`/exam/kit`, examListHandler);
+  }, [sendGetRequest]);
 
   return (
     <ContainerUI>
@@ -17,11 +30,14 @@ const ExamList = () => {
       <Grid container spacing={3} columns={6}>
         <Grid component={Link} to="#" item xs={6} sm={3} md={2} sx={{}}>
           <Box sx={{ height: 300, border: 1, borderColor: 'rgba(0, 0, 0, 0.12)', borderRadius: 3 }}>
-              <Typography component="div" margin={2} gutterBottom>
-                TEST1
-              </Typography>
-              <Divider />
-              <Typography component="div" margin={2} variant='h5'>스프링 모의고사</Typography>
+            <Typography component="div" margin={2} gutterBottom>
+              TEST1
+              {isLoading && <LoadingSpinner />}
+              {!isLoading && 
+              <Typography exam={exam}/>}
+            </Typography>
+            <Divider />
+            <Typography component="div" margin={2} variant='h5'>스프링 모의고사</Typography>
           </Box>
         </Grid>
       </Grid>
