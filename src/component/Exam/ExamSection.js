fa2c03a03bd4aa9b501867ac89c2bd8dc55b9bcc
N3theri9N <nnea5215@gmail.com>
2022-12-19 17:25:43 +0900

    Timer
    - 타이머 추가 ( 기본 10분 )
    - 완료 되었을 때 로그만 찍히도록 대응 ( 플로우 확인 필요 )

--- src/component/Exam/ExamSection.js
+++ src/component/Exam/ExamSection.js
@@ -13,6 +13,7 @@ import ContainerUI from '../UI/ContainerUI';
 import AnswerForm from './AnswerForm';
 import { useParams, useNavigate } from 'react-router-dom';
 import LoadingSpinner from '../UI/LoadingSpinner';
+import Timer from './Timer';
 
 const ExamSection = () => {
   const inputRef = useRef();
@@ -92,6 +93,7 @@ const ExamSection = () => {
             changeIdxHandler={changeIdxHandler}
             length={length}
           />
+          <Timer initSeconds={600} />
           <Divider />
           <Box padding={2}>
             {exam.length > 0 && (
