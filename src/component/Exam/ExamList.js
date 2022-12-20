67e320967abad7fa10ac8a54423d4a85984e531c
lee-ji-hong <zhffktkdlekghksxk@naver.com>
2022-12-20 10:13:47 +0900

    Update ExamList.js

--- src/component/Exam/ExamList.js
+++ src/component/Exam/ExamList.js
@@ -1,30 +1,51 @@
+import { useState, useEffect } from 'react';
 import ContainerUI from '../UI/ContainerUI';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
-import Divider from  '@mui/material/Divider';
+import Divider from '@mui/material/Divider';
+import useHttpRequest from '../../hook/use-http';
+// import LoadingSpinner from '../UI/LoadingSpinner';
 import { Link } from 'react-router-dom';
+
 
 // import { useNavigate } from 'react-router-dom';
 const ExamList = () => {
-  // const navigate = useNavigate();
+
+  const [examList, setExamList] = useState([]);
+  const { sendGetRequest } = useHttpRequest()
+
+  useEffect(() => {
+    const examListHandler = (data) => {
+      console.log(data.data)
+      if (data.success) {
+        setExamList(data.data.map(i => { return { "id": i.id, "title": i.title } }))
+      }
+    }
+    sendGetRequest('/exam/kit', examListHandler)
+  }, [sendGetRequest])
 
   return (
     <ContainerUI>
       <Typography component="h1" variant="h5" fontWeight="bold">
         모의고사
       </Typography>
-      <Grid container spacing={3} columns={6}>
-        <Grid component={Link} to="#" item xs={6} sm={3} md={2} sx={{}}>
-          <Box sx={{ height: 300, border: 1, borderColor: 'rgba(0, 0, 0, 0.12)', borderRadius: 3 }}>
-              <Typography component="div" margin={2} gutterBottom>
-                TEST1
-              </Typography>
-              <Divider />
-              <Typography component="div" margin={2} variant='h5'>스프링 모의고사</Typography>
-          </Box>
-        </Grid>
-      </Grid>
+      <Grid container marginTop={3} spacing={3} columns={6}>{
+        examList.map(i => {
+          const examId = i.id;
+          const title = i.title;
+          return (
+            <Grid key={examId} component={Link} to={`/exam/${examId}`} item xs={6} sm={3} md={2} sx={{ textDecoration: "none", color: "inherit" }}>
+              <Box sx={{ height: 300, border: 1, borderColor: 'rgba(0, 0, 0, 0.12)', borderRadius: 3 }}>
+                <Typography component="div" margin={2} gutterBottom>
+                  TEST{examId}
+                </Typography>
+                <Divider />
+                <Typography component="div" margin={2} variant='h5'>{title}</Typography>
+              </Box>
+            </Grid>);
+        })
+      }</Grid>
       {/* <Button variant="outlined" onClick={() => navigate('/exam/frontend')}>
         FRONTEND
       </Button>
