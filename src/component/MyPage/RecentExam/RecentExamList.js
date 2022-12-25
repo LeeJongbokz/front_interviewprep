e6751a2ea1f1ef5c92139420574dca391520e1fa
lee-ji-hong <zhffktkdlekghksxk@naver.com>
2022-12-26 02:45:34 +0900

    Feat: "최근 푼 모의고사 페이지 생성"

---
+++ src/component/MyPage/RecentExam/RecentExamList.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import Box from '@mui/material/Box';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Table from '@mui/material/Table';
+import RecentExamTable from './RecentExamTable';
+import { Link } from 'react-router-dom';
+
+
+const RecentProblemList = ({ memberInfo }) => {
+
+  const [value, setValue] = useState(0);
+  console.log(memberInfo);
+
+  const handleRecentProblem = (e, newValue) => {
+    setValue(newValue);
+    console.log(newValue);
+  }
+
+  const tableComponent = () => {
+    if (memberInfo.answers) {
+      return <RecentExamTable memberInfo={memberInfo} />;
+    }
+  }
+
+  return (
+    <div>
+
+      <Box>
+        <Tabs value={value} onChange={handleRecentProblem} >
+          <Tab label="연습 문제" component={Link} to="/RecentProblem" />
+          {/* {loading && <LoadingSpinner />} */}
+          <Tab label="모의고사" component={Link} to="/RecentExam" />
+        </Tabs>
+      </Box>
+      <Box>
+        <Table>
+          {tableComponent()}
+        </Table>
+      </Box>
+    </div>
+  )
+}
+
+export default RecentProblemList;
---
+++ src/component/MyPage/RecentExam/RecentExamTable.js
@@ -0,0 +1,60 @@
+import TableRow from '@mui/material/TableRow';
+import { useNavigate } from 'react-router-dom';
+import TableCell from '@mui/material/TableCell';
+import TableBody from '@mui/material/TableBody';
+import classes from '../../Test/Problem.module.css';
+
+
+const RecentExamTable = ({ memberInfo }) => {
+  console.log(memberInfo.answers);
+  const navigate = useNavigate();
+
+  const timeForToday = (value) => {
+    const today = new Date();
+    const timeValue = new Date(value);
+
+    const betweenTime = Math.floor((today.getTime() - timeValue.getTime()) / 1000 / 60);
+    if (betweenTime < 1) return '방금전';
+    if (betweenTime < 60) {
+      return `${betweenTime}분전`;
+    }
+
+    const betweenTimeHour = Math.floor(betweenTime / 60);
+    if (betweenTimeHour < 24) {
+      return `${betweenTimeHour}시간전`;
+    }
+
+    const betweenTimeDay = Math.floor(betweenTime / 60 / 24);
+    if (betweenTimeDay < 365) {
+      return `${betweenTimeDay}일전`;
+    }
+
+    return `${Math.floor(betweenTimeDay / 365)}년전`;
+  }
+  timeForToday()
+
+  return (
+    <TableBody>
+      <TableRow className={classes.row}>
+        <TableCell component="th" scope="row">번호</TableCell>
+        <TableCell>내용</TableCell>
+        <TableCell>날짜</TableCell>
+      </TableRow>
+      {memberInfo.answers.map((item) => {
+        console.log(item)
+        return (
+          <>
+            <TableRow hover className={classes.row} onClick={() => navigate(`/test/${item["@id"]}`)}>
+              <TableCell component="th" scope="row">{item.id}</TableCell>
+              <TableCell>{item.content}</TableCell>
+              <TableCell>{item.modifiedDate}</TableCell>
+            </TableRow>
+
+          </>
+        )
+      })}
+    </TableBody>
+  );
+};
+
+export default RecentExamTable;
