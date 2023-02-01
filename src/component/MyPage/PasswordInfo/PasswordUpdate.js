0d734604eee345c65d813d32f9fdf016317f30ca
lee-ji-hong <zhffktkdlekghksxk@naver.com>
2023-02-01 17:47:55 +0900

    Feat: "비밀번호가 일치하지 않을 때 alert 메세지 띄워주는 기능 구현중"

--- src/component/MyPage/PasswordInfo/PasswordUpdate.js
+++ src/component/MyPage/PasswordInfo/PasswordUpdate.js
@@ -1,68 +1,89 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
 import useHttpRequest from '../../../hook/use-http';
+import Alert from '@mui/material/Alert';
+import Stack from '@mui/material/Stack';
 
 
 const PasswordUpdate = ({ memberInfo }) => {
 
   // console.log(memberInfo);
+  const { sendGetRequest } = useHttpRequest();
+
   const [inputs, setInputs] = useState({
     password: memberInfo.password,
-    newPassword: memberInfo.password,
-    confirmPassword: memberInfo.password
+    newPassword: null,
+    confirmPassword: null
   });
-  const [query,setQuery] = useState([]); 
+  const [query, setQuery] = useState([]);
   const { sendPutRequest } = useHttpRequest();
-  const {  password, newPassword, confirmPassword } = inputs; // 비구조화 할당을 통해 값 추출
+  const { password, newPassword, confirmPassword } = inputs; // 비구조화 할당을 통해 값 추출
+
+  useEffect(() => {
+    const examListHandler = data => {
+      if (data.success) {
+        console.log(inputs)
+        console.log('성공ㅇㅇㅇㅇㅇㅇㅇㅇㅇ')
+        successComponent()
+
+      }
+    };
+    sendGetRequest('/members/userInfo', examListHandler);
+    // sendPutRequest('/members/password/change',examListHandler)
+    // successComponent()
+  }, [sendGetRequest]);
+
+  const successComponent = () => {
+
+    console.log('alal')
+    return (
+      <Stack sx={{ width: '100%' }} spacing={2}>
+        <Alert severity="error">비밀번호를 다시 입력해주세요</Alert>
+      </Stack>
+    )
+
+  }
 
   const onChange = (e) => {
-
-    const { value, password, newPassword, confirmPassword } = e.target; // 우선 e.target 에서 name 과 value 를 추출
-    setInputs({
-      ...inputs, // 기존의 input 객체를 복사한 뒤 // name 키를 가진 값을 value 로 설정
-      [password]: value,
-      [newPassword]: value,
-      [confirmPassword]: value,
+    const { value, id: targetId } = e.target;
+    setInputs((prevState) => {
+      return { ...prevState, [targetId]: value }
     });
     setQuery({
       ...query,
-      [e.target.id]:value
+      [targetId]: value
     })
-    // console.log(e.target)
-    // console.log(value)
-    // console.log(inputs)
-    // console.log(query)
+    console.log(query)
   };
   const passwordUpdateHandler = () => {
-    // console.log(newPassword)
-    // console.log(password)
-    
+
     sendPutRequest({
       endpoint: '/members/password/change',
       bodyData: {
         ...query
-        // password: password
-        // newPassword: newPasswordS
-        // confirmPassword: confirmPassword
       },
+
     });
   };
 
   const onReset = () => {
     setInputs({
-      password: memberInfo.password,
-      newPassword: memberInfo.newPassword,
-      confirmPassword: memberInfo.confirmPassword
+      ...inputs,
+      password: '',
+      newPassword: '',
+      confirmPassword: ''
     })
+    console.log(inputs)
+    console.log(memberInfo.password)
   };
 
   return (
     <div>
       <Typography component="h5" fontWeight="800" >
-        비밀번호
+        현재 비밀번호
       </Typography>
       <TextField
         id="password"
@@ -75,6 +96,9 @@ const PasswordUpdate = ({ memberInfo }) => {
         value={password}
         onChange={onChange}
       />
+      <div>
+        {successComponent()}
+      </div>
       <Typography component="h5" fontWeight="800" >
         새 비밀번호
       </Typography>
@@ -87,7 +111,6 @@ const PasswordUpdate = ({ memberInfo }) => {
         fullWidth
         value={newPassword}
         onChange={onChange}
-        variant="standard"
       />
       <Typography component="h5" fontWeight="800" >
         새 비밀번호 확인
@@ -102,7 +125,6 @@ const PasswordUpdate = ({ memberInfo }) => {
           fullWidth
           value={confirmPassword}
           autoFocus
-          variant="standard"
           onChange={onChange}
         />
       </Box>
